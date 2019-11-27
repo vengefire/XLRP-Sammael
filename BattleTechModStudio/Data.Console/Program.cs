1ab856e10f28c58ffeed872a6b603e3b5617836b
vengefire <vengefire@gmail.com>
2019-11-27 15:48:09 +0800

    Added mod inter-dependency validation

--- BattleTechModStudio/Data.Console/Program.cs
+++ BattleTechModStudio/Data.Console/Program.cs
@@ -14,8 +14,15 @@ namespace Data.Console
 
             System.Console.WriteLine($"Summary for mods loaded from [{sourceDirectory}]:\r\n" +
                                      $"Mods Loaded - {modCollection.Mods.Count}\r\n" +
-                                     $"Invalid Mods - {modCollection.Mods.Count(mod => !mod.IsValid)}\r\n" +
-                                     $"Press any key to continue...");
+                                     $"Invalid Mods - {modCollection.Mods.Count(mod => !mod.IsValid)}\r\n");
+
+            modCollection.Mods.Where(mod => !mod.IsValid).ToList().ForEach(mod =>
+            {
+                System.Console.WriteLine($"Mod [{mod.Name}] is invalid for the following reasons:");
+                System.Console.WriteLine($"{string.Join("\r\n", mod.InvalidReasonList)}\r\n");
+            });
+
+            System.Console.WriteLine("Press any key to exit...");
             System.Console.ReadKey();
         }
     }
--- BattleTechModStudio/Data.Core/ModObjects/ModCollection.cs
+++ BattleTechModStudio/Data.Core/ModObjects/ModCollection.cs
@@ -28,40 +28,65 @@ namespace Data.Core.ModObjects
             // Phase 1 - Set object inter-dependencies...
             Mods
                 .AsParallel().ForAll(
-                //.ToList().ForEach(
+                    //.ToList().ForEach(
                     mod =>
-            {
-                if (mod.ConflictsWithModNames != null && mod.ConflictsWithModNames.Any())
-                {
-                    mod.ConflictsWith = Mods.Where(mod1 => mod.ConflictsWithModNames.Contains(mod1.Name)).ToList();
-                    var conflictingMods = mod.ConflictsWith.Select(mod1 => mod1.Name);
-                    conflictingMods.ToList().ForEach(s => mod.InvalidReasonList.Add($"Conflicting dependency [{s}]"));
-                }
+                    {
+                        if (mod.ConflictsWithModNames != null && mod.ConflictsWithModNames.Any())
+                        {
+                            mod.ConflictsWith = Mods.Where(mod1 => mod.ConflictsWithModNames.Contains(mod1.Name)).ToList();
+                            var conflictingMods = mod.ConflictsWith.Select(mod1 => mod1.Name);
+                            conflictingMods.ToList().ForEach(s => mod.InvalidReasonList.Add($"Conflicting dependency [{s}]"));
+                        }
 
-                if (mod.OptionallyDependsOnModNames != null && mod.OptionallyDependsOnModNames.Any())
-                {
-                    mod.OptionallyDependsOn = Mods.Where(mod1 => mod.OptionallyDependsOnModNames.Contains(mod1.Name)).ToList();
-                }
+                        if (mod.OptionallyDependsOnModNames != null && mod.OptionallyDependsOnModNames.Any())
+                        {
+                            mod.OptionallyDependsOn = Mods.Where(mod1 => mod.OptionallyDependsOnModNames.Contains(mod1.Name)).ToList();
+                        }
 
-                if (mod.DependsOnModNames != null && mod.DependsOnModNames.Any())
-                {
-                    mod.DependsOn = Mods.Where(mod1 => mod.DependsOnModNames.Contains(mod1.Name)).ToList();
-                    var missingDependencies = mod.DependsOnModNames.Except(mod.DependsOn.Select(mod1 => mod1.Name));
-                    missingDependencies.ToList().ForEach(s => mod.InvalidReasonList.Add($"Missing dependency [{s}]"));
-                }
+                        if (mod.DependsOnModNames != null && mod.DependsOnModNames.Any())
+                        {
+                            mod.DependsOn = Mods.Where(mod1 => mod.DependsOnModNames.Contains(mod1.Name)).ToList();
+                            var missingDependencies = mod.DependsOnModNames.Except(mod.DependsOn.Select(mod1 => mod1.Name));
+                            missingDependencies.ToList().ForEach(s => mod.InvalidReasonList.Add($"Missing dependency [{s}]"));
+                        }
 
-                mod.IsValid = !mod.InvalidReasonList.Any();
-            });
+                        mod.IsValid = !mod.InvalidReasonList.Any();
+                    });
 
             // Phase 2 - Check each mods dependency validity. We can only do this after setting mod inter-dependency in phase 1
             Mods
-                .AsParallel().ForAll(
+                .Where(mod => mod.DependsOn.Any())
+                //.AsParallel().ForAll(
+                .ToList().ForEach(
                     mod =>
+                    {
+                        var dependencyTree = GetModDependenciesWithLevel(mod, 1, null);
+                        dependencyTree.Sort((o, o1) => o1.level.CompareTo(o.level));
+
+                        var invalidDependencies = dependencyTree.Where(o => !o.mod.IsValid).Select(o => o.mod.Name);
+                        invalidDependencies.ToList().ForEach(s => mod.InvalidReasonList.Add($"Dependency [{s}] is invalid."));
+                        mod.IsValid = !mod.InvalidReasonList.Any();
+
+                        /*var invalidDependencies = mod.DependsOn.Where(mod1 => !mod1.IsValid).Select(mod1 => mod1.Name);
+                        invalidDependencies.ToList().ForEach(s => mod.InvalidReasonList.Add($"Dependency [{s}] is invalid."));
+                        mod.IsValid = !mod.InvalidReasonList.Any();*/
+                    });
+        }
+
+        private List<dynamic> GetModDependenciesWithLevel(Mod mod, int level, List<dynamic> dependencies)
+        {
+            dependencies = dependencies ?? new List<dynamic>();
+            mod.DependsOn.ForEach(mod1 =>
             {
-                var invalidDependencies = mod.DependsOn.Where(mod1 => !mod1.IsValid).Select(mod1 => mod1.Name);
-                invalidDependencies.ToList().ForEach(s => mod.InvalidReasonList.Add($"Dependency [{s}] is invalid."));
-                mod.IsValid = !mod.InvalidReasonList.Any();
+                dependencies.Add(new {mod = mod1, level = level});
+                GetModDependenciesWithLevel(mod1, level + 1, dependencies);
             });
+            return dependencies;
+        }
+
+        public void ProcessModLoadOrder()
+        {
+            var modsToLoad = Mods.Where(mod => mod.IsValid);
         }
     }
 }
